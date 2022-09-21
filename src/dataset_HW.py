eadf149c8edd89f59c13cd8c292fda1b7c2ad149
Won Jeong <59783645+mallang327@users.noreply.github.com>
2022-09-21 17:06:55 +0900

    Update and rename dataset.py to dataset_HW.py

--- src/dataset_HW.py
+++ src/dataset_HW.py
@@ -11,8 +11,10 @@ class DatasetMix(torch.utils.data.Dataset):
     def __init__(self, dir_clean,path_noise,sec=1.0,device="cuda:0"):
         self.list_clean = glob.glob(os.path.join(dir_clean,"*.wav"))
 
-        self.noise,_ = librosa.load(path_noise,sr=16000)
+        # Noise 파일 16KHz로 불러오기
+        self.noise,_ = 
 
+        # 1초
         self.len_item = int(sec*16000)
         self.device = device
 
@@ -20,10 +22,11 @@ class DatasetMix(torch.utils.data.Dataset):
 
     def __getitem__(self, idx):
 
-        # clean matching
+        # clean 파일 16KHz로 불러오기
         path_clean = self.list_clean[idx]
-        tmp_clean,_ = librosa.load(path_clean,sr=16000)
+        tmp_clean,_ = 
 
+        # data length 맞추기
         if len(tmp_clean) > self.len_item :
             idx_clean = np.random.randint(len(tmp_clean)-self.len_item)
             tmp_clean = tmp_clean[idx_clean:idx_clean + self.len_item]
@@ -33,19 +36,22 @@ class DatasetMix(torch.utils.data.Dataset):
         else :
             pass
         
-        # noise sampling
+        # noise sampling, noise length clean과 똑같이 맞추기
         idx_noise = np.random.randint(len(self.noise)-self.len_item)
         tmp_noise = self.noise[idx_noise:idx_noise+self.len_item]
 
         # SNR
+        ## 0 ~ 10
         SNR = np.random.rand()*10
-        energy_clean = np.sum(np.power(tmp_clean,2))
-        energy_noise = np.sum(np.power(tmp_noise,2))
 
-        normal = np.sqrt(energy_clean)/np.sqrt(energy_noise)
-        weight = normal/np.sqrt(np.power(10,SNR/10))
+        ## DB scale로 나타내기 위해 energy 확인
+        energy_clean = # tmp_clean의 energy
+        energy_noise = # tmp_noise의 energy
 
-        tmp_noise *=weight
+        normal = # clean과 Noise 비율에 맞춰 Normalizing
+        weight = # Hint: normal / (...)
+
+        tmp_noise *=weight # 앞서 구한 SNR만큼의 세기로 Noise 세기 변경
 
         # Mixing
         noisy = tmp_clean + tmp_noise
@@ -56,13 +62,15 @@ class DatasetMix(torch.utils.data.Dataset):
         noisy = noisy/scaling_factor
         clean = tmp_clean/scaling_factor
 
+        # Type 변경 (numpy array --> torch tensor)
         noisy = torch.from_numpy(noisy)
         clean = torch.from_numpy(clean)
 
-        noisy_spec =  torch.stft(noisy,n_fft=512,return_complex=True,center=True)
+        # STFT Domain change (Spectrogram)
+        noisy_spec =  # n_fft = 512
 
-        noisy_mag = torch.abs(noisy_spec)
-        noisy_phase = torch.angle(noisy_spec)
+        noisy_mag =   # noisy_spec에서 추출
+        noisy_phase = # noisy_spec에서 추출
 
         noisy_mag = torch.unsqueeze(noisy_mag,dim=0)
         noisy_phase = torch.unsqueeze(noisy_phase,dim=0)
@@ -96,14 +104,15 @@ class DatasetFix(torch.utils.data.Dataset):
 
         name_clean = path_clean.split("/")[-1]
 
-        noisy,_ = librosa.load(os.path.join(self.dir_dataset,"noisy",name_clean),sr=16000)
+        noisy,_ = # Hint: Noisy폴더 내의 name_clean 변수에 해당하는 파일을 16KHz로 load
         
+        # Type 변경 (numpy array --> torch tensor)
         noisy = torch.from_numpy(noisy)
         clean = torch.from_numpy(clean)
 
-        noisy_spec =  torch.stft(noisy,n_fft=512,return_complex=True,center=True)
-        noisy_mag = torch.abs(noisy_spec)
-        noisy_phase = torch.angle(noisy_spec)
+        noisy_spec =  # DatasetMix와 동일 ## n_fft = 512
+        noisy_mag = # noisy_spec에서 추출
+        noisy_phase = # noisy_spec에서 추출
 
         noisy_mag = torch.unsqueeze(noisy_mag,dim=0)
         noisy_phase = torch.unsqueeze(noisy_phase,dim=0)
